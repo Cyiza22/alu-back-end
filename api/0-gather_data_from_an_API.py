f4274e8812baf6c614f2ccbc272d6d7db975b12d
Cyiza22 <h.cyiza@alustudent.com>
2023-11-05 21:51:47 +0000

    api

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""
-From task #0, extending the Python script to export data in the CSV format.
+"""Writing a Python script that, using the REST API, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 
-import csv
 import requests
 import sys
 
@@ -15,23 +14,22 @@ if __name__ == "__main__":
     todos_url = "https://jsonplaceholder.typicode.com/todos"
     users_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
 
-    file_content = []
-
     todo_data = requests.get(todos_url).json()
 
-    employee_name = requests.get(users_url).json()["username"]
+    employee_name = requests.get(users_url).json()["name"]
 
+    total_user_todos = 0
+    completed_todos = 0
+    titles = []
     for todo in todo_data:
         if user_id == todo["userId"]:
-            file_content.append(
-                [str(user_id), employee_name, todo["completed"],
-                 todo["title"]])
+            total_user_todos += 1
+            if todo["completed"]:
+                completed_todos += 1
+                titles.append(todo["title"])
 
-    print(file_content)
-    file_name = "{}.csv".format(user_id)
-    with open(file_name, 'w', newline='') as csv_file:
-        write = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for row in file_content:
-            for item in row:
-                str(item)
-            write.writerow(row)
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employee_name, completed_todos, total_user_todos))
+
+    for title in titles:
+        print("\t {}".format(title))
